fc2b73d1b893940ac7a8f8dbec11e9eea275b352
Sudharshan2026 <bshanth0206@gmail.com>
2025-08-30 00:12:46 +0530

    vercel deployment henhace and the2

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        visualizer({
-            filename: 'dist/stats.html',
-            open: true,
-            gzipSize: true,
-            brotliSize: true,
-        }),
     ],
     resolve: {
         alias: {
