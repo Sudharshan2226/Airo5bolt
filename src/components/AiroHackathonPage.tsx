db556983dd45b9f0ec704dbe267c0cfda1cf2d63
SUDHARSHAN BAKTHAVATSALAM <sec23ad073@sairamtap.edu.in>
2025-09-01 09:01:40 +0000

    heloo

--- src/components/AiroHackathonPage.tsx
+++ src/components/AiroHackathonPage.tsx
@@ -294,7 +294,7 @@ const AiroHackathonPage = () => {
                   <li>• Team size: 3 members per team</li>
                   <li>• Problem statement will be revealed only during the event</li>
                   <li>• All designs must be original (plagiarism = disqualification)</li>
-                  <li>• Use of pre-built templates must be declared</li>
+                  <li>• Use of pre-built templates must be prohibited</li>
                   <li>• Teams must present their final design within the time limit</li>
                 </ul>
 
