08a909371a928d1e3cd60132b37ae7653341e690
SUDHARSHAN BAKTHAVATSALAM <sec23ad073@sairamtap.edu.in>
2025-09-01 19:37:14 +0530

    chatbot

--- src/components/AiroChatbot.tsx
+++ src/components/AiroChatbot.tsx
@@ -52,7 +52,7 @@ const AiroChatbot = () => {
       twitter: "https://www.linkedin.com/in/prasanna-hari-ram-s-824409333?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
     },
     {
-      name: "S K SANJEEVAN",
+      name: "Gurunath",
       twitter: "https://www.linkedin.com/in/sanjeevansks"
     },
     {
